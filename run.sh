3ace04d9baa6097593af3ec21d98a345cc1db1fb
bruce cheung <bruce.cheung@emplustech.com>
2025-10-03 20:00:12 +0800

    [bpi-r3] Add run.sh to support building model bpi-r3

---
+++ run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function usage {
+    echo "Usage: $0 <model>"
+    echo "Available models: bpi-r3"
+}
+
+[ $# -ne 1 ] && usage && exit 1
+
+model=$1
+
+case $model in
+    bpi-r3) ;;
+    *) echo "Unsupported model: $model" && usage && exit 1 ;;
+esac
+
+time {
+    sudo umount -l ../build
+    sudo rm -rf ../build ../SD
+    make clean
+    git restore build.conf
+    sed -ri "s/^#(board=${model})/\1/" build.conf
+    ./build.sh importconfig
+    ./build.sh build
+    ./build.sh pack
+}
