e1289af9dd6390f7272b4147ae1bad0e292688af
bruce cheung <bruce.cheung@emplustech.com>
2025-10-09 20:19:43 +0800

    [bpi-r4] Patch run.sh to support building model bpi-r4

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 function usage {
     echo "Usage: $0 <model>"
-    echo "Available models: bpi-r3"
+    echo "Available models: bpi-r3 bpi-r4"
 }
 
 [ $# -ne 1 ] && usage && exit 1
@@ -11,6 +11,7 @@ model=$1
 
 case $model in
     bpi-r3) ;;
+    bpi-r4) ;;
     *) echo "Unsupported model: $model" && usage && exit 1 ;;
 esac
 
